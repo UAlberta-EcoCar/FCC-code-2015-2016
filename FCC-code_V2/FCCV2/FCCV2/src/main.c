8f3e129b5fa658723db8e03e87e5e86905853f75
mesanu <esanu@ualberta.ca>
2015-10-24 13:48:05 -0600

    ast working

---
+++ FCC-code_V2/FCCV2/FCCV2/src/main.c
@@ -0,0 +1,65 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/**
+ * \mainpage User Application template doxygen documentation
+ *
+ * \par Empty user application template
+ *
+ * Bare minimum empty user application template
+ *
+ * \par Content
+ *
+ * -# Include the ASF header files (through asf.h)
+ * -# "Insert system clock initialization code here" comment
+ * -# Minimal main function that starts with a call to board_init()
+ * -# "Insert application code here" comment
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+/*
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+#include <asf.h>
+#include "FuelCell_Outputs.h"
+
+uint32_t a;
+char b;
+unsigned int g;
+unsigned long counta;
+int main (void)
+{
+	board_init();
+	DLED_GPERS=(LED0 | LED1 | LED2 | LED3);
+	DLED_ODERS=(LED0 | LED1 | LED2 | LED3);
+	
+	g=0;
+	a=0;
+	/* Insert system clock initialization code here (sysclk_init()). */
+	
+	
+	
+	counta=0;
+	b=ast_init_counter(&AVR32_AST,AST_OSC_RC,AST_PSEL_32KHZ_1HZ,counta);
+	ast_enable(&AVR32_AST);
+	while(1){
+		if((ast_get_counter_value(&AVR32_AST)%7)==0){
+			DLED_OVRS |= LED0;
+		}
+		if((ast_get_counter_value(&AVR32_AST)%13)==0){
+			DLED_OVRC |= LED0;
+		}
+	}
+
+
+	
+	/* Insert application code here, after the board has been initialized. */
+}
